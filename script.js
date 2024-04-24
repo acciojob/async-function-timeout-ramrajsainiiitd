33e7b874a897fbf7215063b65e685bbadf124e84
ramrajsainiiitd <109168257+ramrajsainiiitd@users.noreply.github.com>
2024-04-25 00:04:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click',()=>{
+const text = document.getElementById('text').value;
+const delay = document.getElementById('delay').value;
+const output = document.getElementById('output');
+
+console.log('ramraj', text, delay);
+    setTimeout(()=>{
+        output.textContent = text;
+    }, delay)
+})
